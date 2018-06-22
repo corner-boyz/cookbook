1cdc59613b935b3b3d42a6025de6a518c9c95f18
Carter Robinson <cartertrobinson@gmail.com>
2018-06-22 16:41:16 -0500

    Merging from dev

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+     <View style={styles.container}>
+      <Text>Butthole!</Text>
+      <Text>Changes you make will automatically reload.</Text>
+      <Text>Shake your phone to open the developer menu.</Text>
+    </View>
+    // <Router>
+    //   <Scene key="root">
+    //     {/* Tab Container */}
+    //     <Scene
+    //       key="tabbar"
+    //       tabs={true}
+    //       tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+    //       >
+    //       {/* Tab and it's scenes */}
+    //       <Scene key="osu" title="OSU" icon={TabIcon}>
+    //         <Scene 
+    //           key="scarlet"
+    //           // component={ScarletScreen}
+    //           title="Scarlet"
+    //           />
+    //         <Scene
+    //           key="gray"
+    //           //component={GrayScreen}
+    //           title="Gray"
+    //           />
+    //       </Scene>
+    //       {/* Removed for brevity */}
+    //     </Scene>
+    //   </Scene>
+    //   </Router>
     );
   }
 }
