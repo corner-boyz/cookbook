d5f18119e54691a3b1059c3ed372c16477abf9bc
Carter Robinson <cartertrobinson@gmail.com>
2018-06-25 09:53:09 -0500

    Merging from upstream

--- client-components/ingredients.js
+++ client-components/ingredients.js
@@ -10,7 +10,25 @@ import {
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import { parse } from 'recipe-ingredient-parser';
+
 class Ingredients extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: [],
+      text: ''
+    }
+  }
+
+  addIngredient(){
+    let newIngredients = this.state.ingredients.concat(parse(this.state.text));
+    this.setState({
+      ingredients: newIngredients,
+      text: '',
+    });
+  }
+
   static navigationOptions = {
     tabBarColor: 'green',
     tabBarIcon: () => {
@@ -25,6 +43,17 @@ class Ingredients extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Here are your Ingredients</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+            onPress={this.addIngredient}
+            title="Add Ingredient"
+            color="#841584"
+            accessibilityLabel="Add Ingredient"
+          />
       </View>
     )
   }
